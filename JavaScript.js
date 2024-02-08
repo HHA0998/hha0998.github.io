e5a81b044a7079535036df38401a485fba1da73a
HHA0998 <HHA0998@student.hhacademy.vic.edu.au>
2024-02-08 15:02:42 +1100

    update

--- JavaScript.js
+++ JavaScript.js
@@ -1,2 +1,21 @@
-addEventListener("click", (event) => {});
-onclick = (event => {});
+var hor = 1;
+var ver = 2;
+var Debug;
+
+const pageX = document.getElementById("x");
+const pageY = document.getElementById("y");
+
+function updateDisplay(event) {
+  pageX.innerText = event.pageX;
+  pageY.innerText = event.pageY;
+}
+
+addEventListener("mousemove", updateDisplay, false);
+addEventListener("mouseenter", updateDisplay, false);
+addEventListener("mouseleave", updateDisplay, false);
+
+document.getElementById("Mh").innerHTML = hor;
+document.getElementById("Mv").innerHTML = ver;
+console.log(ver);
+
+
